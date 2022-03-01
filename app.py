44543379f489d54f53216b8d3fc2a83910f86413
NiveskZ <krsmjogos@outlook.com>
2022-02-28 21:11:48 -0300

    Added a default city

--- app.py
+++ app.py
@@ -6,17 +6,21 @@ app = Flask(__name__)
 @app.route('/')
 def getWeather():
     url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=2b8bd8ea752bde2fb17774a395b3ffbe"
-    country = request.values.get("country")
-    r = requests.get(url.format(country))
-    country_weather = json.loads(r.text)
+    location = request.values.get("country")
+
+    if location == None:
+        location = "sao paulo"
+
+    r = requests.get(url.format(location))
+    location_weather = json.loads(r.text)
 
     weather = {
 
-        'local_name': country_weather['name'],
-        'temp': country_weather['main']['temp'],
-        'feels_like': country_weather['main']['feels_like'],
-        'description': country_weather['weather'][0]['description'],
-        'icon': "http://openweathermap.org/img/wn/" + country_weather['weather'][0]['icon'] + ".png"
+        'local_name': location_weather['name'],
+        'temp': location_weather['main']['temp'],
+        'feels_like': location_weather['main']['feels_like'],
+        'description': location_weather['weather'][0]['description'],
+        'icon': "http://openweathermap.org/img/wn/" + location_weather['weather'][0]['icon'] + ".png"
 
     }
     return render_template("weather.html", weather=weather)
